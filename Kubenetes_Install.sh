da270fdc8c71472228fd16c4fb5397bce98aca8f
morriswangx <46831321+morriswangx@users.noreply.github.com>
2022-06-12 07:25:24 -0400

    Update Kubenetes_Install.sh

--- Kubenetes_Install.sh
+++ Kubenetes_Install.sh
@@ -1,3 +1,4 @@
+#
 sudo apt update
 
 # installing the apt-transport-https package which enables working with http and https in Ubuntu’s repositories. Also, install curl as it will be necessary for the next steps.
@@ -17,27 +18,15 @@ sudo apt update
 # re installed on all nodes.
 sudo apt-get install -y kubelet kubeadm kubectl kubernetes-cni
 
-# Kubernetes fails to function in a system that is using swap memory. Hence, it must be disabled in the master node and all worker nodes. Execute the followin
-# g command to disable swap memory:
-sudo swapoff -a
-
 # Letting Iptables See Bridged Traffic
-# For the master and worker nodes to correctly see bridged traffic, you should ensure net.bridge.bridge-nf-call-iptables is set to 1 in your config. First, en
-sure the br_netfilter module is loaded. You can confirm this by issuing the command:
+# For the master and worker nodes to correctly see bridged traffic, you should ensure net.bridge.bridge-nf-call-iptables is set to 1 in your config. First, ensure the br_netfilter module is loaded. You can confirm this by issuing the command:
 
 lsmod | grep br_netfilter
 sudo modprobe br_netfilter
 sudo sysctl net.bridge.bridge-nf-call-iptables=1
 
 # Changing Docker Cgroup Driver
-# By default, Docker installs with “cgroupfs” as the cgroup driver. Kubernetes recommends that Docker should run with “systemd” as the driver. If you skip thi
-# s step and try to initialize the kubeadm in the next step, you will get the following warning in your terminal:
-
-# [preflight] Running pre-flight checks
-#    [WARNING IsDockerSystemdCheck]: detected "cgroupfs" as the Docker cgroup driver. The recommended driver is "systemd". Please follow the guide at https://
-kubernetes.io/docs/setup/cri/
-
-# On both master and worker nodes, update the cgroupdriver with the following commands:
+# By default, Docker installs with “cgroupfs” as the cgroup driver. Kubernetes recommends that Docker should run with “systemd” as the driver. On both master and worker nodes, update the cgroupdriver with the following commands:
 
 sudo mkdir /etc/docker
 cat <<EOF | sudo tee /etc/docker/daemon.json
